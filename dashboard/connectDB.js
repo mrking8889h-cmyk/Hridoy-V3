ccb265d6e326d3c161c8505decd799d66f9b5dfc
hridoyhossen48360 <alaminkhan123466hh@gmail.com>
2026-06-17 22:02:57 +0600

    Add files via upload

--- dashboard/connectDB.js
+++ dashboard/connectDB.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
-const dirConfig = path.join(`${__dirname}/../${process.env.NODE_ENV === 'development' ? 'config.dev.json' : 'config.json'}`);
-const dirConfigCommands = path.join(`${__dirname}/../${process.env.NODE_ENV === 'development' ? 'configCommands.dev.json' : 'configCommands.json'}`);
+const dirConfig = path.join(`${__dirname}/../config.json`);
+const dirConfigCommands = path.join(`${__dirname}/../configCommands.json`);
 
 global.GoatBot = {
 	config: require(dirConfig),
@@ -12,26 +12,32 @@ global.client = {
 	database: {
 		creatingThreadData: [],
 		creatingUserData: [],
-		creatingDashBoardData: []
+		creatingDashBoardData: [],
+		creatingBankData: []
 	}
 };
 global.db = {
 	allThreadData: [],
 	allUserData: [],
-	globalData: []
+	globalData: [],
+	allBankData: []
 };
 
 module.exports = async function () {
 	const controller = await require(path.join(__dirname, "..", "database/controller/index.js"))(null); // data is loaded here
-	const { threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData } = controller;
+	const { threadModel, userModel, dashBoardModel, globalModel, bankModel, staiHistoryModel, threadsData, usersData, dashBoardData, globalData, bankData, staiHistoryData } = controller;
 	return {
 		threadModel,
 		userModel,
 		dashBoardModel,
 		globalModel,
+		bankModel,
+		staiHistoryModel,
 		threadsData,
 		usersData,
 		dashBoardData,
-		globalData
+		globalData,
+		bankData,
+		staiHistoryData
 	};
 };
